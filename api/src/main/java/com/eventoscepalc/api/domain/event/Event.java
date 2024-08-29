3897cf3600b80bfeec139fb685d0f39d1b1b3f8d
Lucas <lucascardoso213@gmail.com>
2024-08-28 21:27:26 -0300

    feat: Criação da classe de evento

---
+++ api/src/main/java/com/eventoscepalc/api/domain/event/Event.java
@@ -0,0 +1,39 @@
+package com.eventoscepalc.api.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID ID;
+
+    private String title;
+
+    private String description;
+
+    private String imgUrl;
+
+    private String eventUrl;
+
+    private Boolean remote;
+
+    private Date date;
+
+
+}
